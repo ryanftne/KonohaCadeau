f95d015b545f11eb0fbd7028ad5289a110b287d2
ryanftne <ryan-fontaine@hotmail.fr>
2021-10-06 17:33:13 +0200

    home page

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ConnexionComponent } from './connexion/connexion.component';
+import { HomeComponent } from './home/home.component';
+import { ProductComponent } from './product/product.component';
+
+const routes: Routes = [
+  {path:'home',component:HomeComponent},
+  {path:'product',component:ProductComponent},
+  {path:'connexion',component:ConnexionComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
