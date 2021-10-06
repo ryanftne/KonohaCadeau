30d0e6eb682b164e696572cf5414e22257346b15
Ryan <56584078+ryanftne@users.noreply.github.com>
2021-10-06 16:57:22 +0200

    Add files via upload

---
+++ konoha_cadeau.sql
@@ -0,0 +1,317 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.7
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : localhost:3306
+-- Généré le : mer. 06 oct. 2021 à 14:29
+-- Version du serveur :  5.7.32
+-- Version de PHP : 7.4.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+--
+-- Base de données : `konoha_cadeau`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `cart`
+--
+
+CREATE TABLE `cart` (
+  `id` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `product_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `cart`
+--
+
+INSERT INTO `cart` (`id`, `quantity`, `product_id`) VALUES
+(1, 5, 1),
+(2, 2, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `cart_user`
+--
+
+CREATE TABLE `cart_user` (
+  `cart_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `cart_user`
+--
+
+INSERT INTO `cart_user` (`cart_id`, `user_id`) VALUES
+(1, 1),
+(2, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `doctrine_migration_versions`
+--
+
+CREATE TABLE `doctrine_migration_versions` (
+  `version` varchar(191) COLLATE utf8_unicode_ci NOT NULL,
+  `executed_at` datetime DEFAULT NULL,
+  `execution_time` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Déchargement des données de la table `doctrine_migration_versions`
+--
+
+INSERT INTO `doctrine_migration_versions` (`version`, `executed_at`, `execution_time`) VALUES
+('DoctrineMigrations\\Version20211006092336', '2021-10-06 09:23:44', 90),
+('DoctrineMigrations\\Version20211006092909', '2021-10-06 09:29:14', 73),
+('DoctrineMigrations\\Version20211006093055', '2021-10-06 09:31:01', 97),
+('DoctrineMigrations\\Version20211006093442', '2021-10-06 09:34:47', 96),
+('DoctrineMigrations\\Version20211006094026', '2021-10-06 09:40:29', 91),
+('DoctrineMigrations\\Version20211006094112', '2021-10-06 09:41:14', 106),
+('DoctrineMigrations\\Version20211006094917', '2021-10-06 09:49:21', 77),
+('DoctrineMigrations\\Version20211006095051', '2021-10-06 09:50:53', 75),
+('DoctrineMigrations\\Version20211006095117', '2021-10-06 09:51:19', 98),
+('DoctrineMigrations\\Version20211006101830', '2021-10-06 10:18:33', 141),
+('DoctrineMigrations\\Version20211006102314', '2021-10-06 10:23:17', 117),
+('DoctrineMigrations\\Version20211006104652', '2021-10-06 10:46:56', 75);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `etat`
+--
+
+CREATE TABLE `etat` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `etat`
+--
+
+INSERT INTO `etat` (`id`, `name`) VALUES
+(1, 'En préparation'),
+(2, 'Envoyée');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `order`
+--
+
+CREATE TABLE `order` (
+  `id` int(11) NOT NULL,
+  `etat_id` int(11) DEFAULT NULL,
+  `created_at` datetime NOT NULL COMMENT '(DC2Type:datetime_immutable)',
+  `updated_at` datetime NOT NULL COMMENT '(DC2Type:datetime_immutable)',
+  `user_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `order`
+--
+
+INSERT INTO `order` (`id`, `etat_id`, `created_at`, `updated_at`, `user_id`) VALUES
+(1, 1, '2021-10-06 12:02:59', '2021-10-06 12:02:59', 1),
+(2, 2, '2021-10-06 12:02:59', '2021-10-06 12:02:59', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `order_product`
+--
+
+CREATE TABLE `order_product` (
+  `order_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `order_product`
+--
+
+INSERT INTO `order_product` (`order_id`, `product_id`) VALUES
+(1, 1),
+(1, 2),
+(2, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `product`
+--
+
+CREATE TABLE `product` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `image` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `price` double NOT NULL,
+  `description` longtext COLLATE utf8mb4_unicode_ci,
+  `category` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `product`
+--
+
+INSERT INTO `product` (`id`, `name`, `image`, `price`, `description`, `category`) VALUES
+(1, '.357 Magnum', 'https://assets.laboutiqueofficielle.com/w_450,q_auto,f_auto/media/products/2019/03/25/y-et-w_177640_LE-BOB-REVERSIBLE_BLACK-WHITE_20190401T101517_02.jpg', 350.99, 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis commodi odit assumenda suscipit hic nemo obcaecati, nostrum aut error fuga exercitationem ratione aspernatur mollitia alias doloribus quaerat voluptates praesentium facere.\r\n', 'Revolver'),
+(2, 'Clavier mécanique - Gamer', 'https://assets.laboutiqueofficielle.com/w_450,q_auto,f_auto/media/products/2019/03/25/y-et-w_177640_LE-BOB-REVERSIBLE_BLACK-WHITE_20190401T101517_02.jpg', 52.99, 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis commodi odit assumenda suscipit hic nemo obcaecati, nostrum aut error fuga exercitationem ratione aspernatur mollitia alias doloribus quaerat voluptates praesentium facere.\r\n', 'Informatique');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `email` varchar(180) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `roles` json NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `address` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `city` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `postal_code` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `user`
+--
+
+INSERT INTO `user` (`id`, `email`, `roles`, `password`, `address`, `city`, `postal_code`) VALUES
+(1, 'antoinevasone@outlook.com', '[\"ROLE_ADMIN\"]', 'Guizmo', '21 quai de l\'ourcq', 'Pantin', 93500),
+(2, 'thomasyalap@outlook.com', '[\"ROLE_USER\"]', 'Pioute', '2 allée des Tulipes', 'Sevran', 93270);
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `cart`
+--
+ALTER TABLE `cart`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `IDX_BA388B74584665A` (`product_id`);
+
+--
+-- Index pour la table `cart_user`
+--
+ALTER TABLE `cart_user`
+  ADD PRIMARY KEY (`cart_id`,`user_id`),
+  ADD KEY `IDX_6276D6701AD5CDBF` (`cart_id`),
+  ADD KEY `IDX_6276D670A76ED395` (`user_id`);
+
+--
+-- Index pour la table `doctrine_migration_versions`
+--
+ALTER TABLE `doctrine_migration_versions`
+  ADD PRIMARY KEY (`version`);
+
+--
+-- Index pour la table `etat`
+--
+ALTER TABLE `etat`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `order`
+--
+ALTER TABLE `order`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `IDX_F5299398D5E86FF` (`etat_id`),
+  ADD KEY `IDX_F5299398A76ED395` (`user_id`);
+
+--
+-- Index pour la table `order_product`
+--
+ALTER TABLE `order_product`
+  ADD PRIMARY KEY (`order_id`,`product_id`),
+  ADD KEY `IDX_2530ADE68D9F6D38` (`order_id`),
+  ADD KEY `IDX_2530ADE64584665A` (`product_id`);
+
+--
+-- Index pour la table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `UNIQ_8D93D649E7927C74` (`email`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `cart`
+--
+ALTER TABLE `cart`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT pour la table `etat`
+--
+ALTER TABLE `etat`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT pour la table `order`
+--
+ALTER TABLE `order`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT pour la table `product`
+--
+ALTER TABLE `product`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT pour la table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `cart`
+--
+ALTER TABLE `cart`
+  ADD CONSTRAINT `FK_BA388B74584665A` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`);
+
+--
+-- Contraintes pour la table `cart_user`
+--
+ALTER TABLE `cart_user`
+  ADD CONSTRAINT `FK_6276D6701AD5CDBF` FOREIGN KEY (`cart_id`) REFERENCES `cart` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `FK_6276D670A76ED395` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE;
+
+--
+-- Contraintes pour la table `order`
+--
+ALTER TABLE `order`
+  ADD CONSTRAINT `FK_F5299398A76ED395` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
+  ADD CONSTRAINT `FK_F5299398D5E86FF` FOREIGN KEY (`etat_id`) REFERENCES `etat` (`id`);
+
+--
+-- Contraintes pour la table `order_product`
+--
+ALTER TABLE `order_product`
+  ADD CONSTRAINT `FK_2530ADE64584665A` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `FK_2530ADE68D9F6D38` FOREIGN KEY (`order_id`) REFERENCES `order` (`id`) ON DELETE CASCADE;
